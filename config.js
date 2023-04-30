637b0d901dcce40129d4864683eda10f716b6ad1
dat2k2 <132198600+dat2k2@users.noreply.github.com>
2023-04-30 17:15:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Gửi cho",
+    introTitle: 'Bé Yun cute phomai que ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
