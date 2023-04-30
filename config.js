7da8fbac67e28f028d2e54f8b578180fb879fa90
dat2k2 <132198600+dat2k2@users.noreply.github.com>
2023-04-30 17:30:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const CONFIG = {
     titleWeb: "Gửi ny tui",
     introTitle: 'Bé Yun cute ',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    introDesc: `Mình cảm ơn cậu nhiều lém óa đã cho mình thêm cơ hội ạ 
+     Hehe
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Mình nhớ cậu lém óa  🥰',
+    desc: 'Cậu tuyệt vời lém cậu biết hem  ',
     btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnNo: 'Thích thật óa  :3',
+    question: 'Mình chỉ nhớ bạn thui ',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    messDesc: 'Mình sẽ thay đổi tích cực hơn óa ',
+    btnAccept: 'Okiiiii nhoa <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
